aac3dc682966a9f40be1294ae9f0e3ff3077e8ee
xuan.liu <xuan.liu@positivegrid.com>
2023-03-12 15:48:34 +0800

    Add project

---
+++ flutter_application_1/lib/BigCard.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(pair.asUpperCase, style: style, semanticsLabel: "${pair.first} ${pair.second}",),
+      ),
+    );
+  }
+}
